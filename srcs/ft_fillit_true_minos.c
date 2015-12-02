fd5d320ec7b91465a7bb5ac6b30933cefcdc91e9
Anne-laure VACHER <avacher@e2r12p9.42.fr>
2015-12-02 18:49:23 +0100

    true mino 1 ok

--- srcs/ft_fillit_true_minos.c
+++ srcs/ft_fillit_true_minos.c
@@ -1,6 +1,6 @@
 #include "fillit.h"
 
-int		t_issquare(char *t_mino)
+int		t_is_square(char *t_mino)
 {
 	int		i;
 
@@ -16,7 +16,7 @@ int		t_issquare(char *t_mino)
 	return (0);
 }
 
-int		t_ispipe(char *t_mino)
+int		t_is_ipipe(char *t_mino)
 {
 	int		i;
 
@@ -28,6 +28,44 @@ int		t_ispipe(char *t_mino)
 			return (1);
 		else if (t_mino[i] == '#' && t_mino[i + 1] == '#' && t_mino[i + 2] == '#'
 				&& t_mino[i + 3] == '#')
+			return (1);
+		else
+			ft_error();
+	}
+	return (0);
+}
+
+int		t_is_s(char *t_mino)
+{
+	int		i;
+
+	i = 0;
+	while (t_mino[i] != '\0')
+	{
+		if (t_mino[i] == '#' && t_mino[i + 1] == '#' && t_mino[i + 4] == '#'
+				&& t_mino[i + 5] == '#')
+			return (1);
+		if (t_mino[i] == '#' && t_mino[i + 5] == '#' && t_mino[i + 6] == '#'
+				&& t_mino[i + 11] == '#')
+			return (1);
+		else
+			ft_error();
+	}
+	return (0);
+}
+
+int		t_isrev_s(char *t_mino)
+{
+	int		i;
+
+	i = 0;
+	while (t_mino[i] != '\0')
+	{
+		if (t_mino[i] == '#' && t_mino[i + 1] == '#' && t_mino[i + 6] == '#'
+				&& t_mino[i + 7] == '#')
+			return (1);
+		if (t_mino[i] == '#' && t_mino[i + 4] == '#' && t_mino[i + 5] == '#'
+				&& t_mino[i + 9] == '#')
 			return (1);
 		else
 			ft_error();
