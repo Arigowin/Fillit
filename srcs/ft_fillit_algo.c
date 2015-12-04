3a409ea1991ca418f3e4014b0b175a8c6b84ac0d
avacher <avacher@student.42.fr>
2015-12-04 11:31:18 +0100

    fonction grille minimum ajoutee

--- srcs/ft_fillit_algo.c
+++ srcs/ft_fillit_algo.c
@@ -1,20 +1,31 @@
 #include "fillit.h"
 #include <stdio.h>
 
+static int		min_square(int t_minos_nb)
+{
+	int		side_len;
 
-char		*t_algo_recu(char *grid, int size)
+	side_len = 4;
+	while ((t_minos_nb * 4) > (side_len * side_len))
+	{
+		side_len++;
+	}
+	return (side_len);
+}
+
+char			*t_algo_recu(char *grid, int size)
 {
 
 }
 
-void		t_algo(char **t_mino)
+void			t_algo(char **t_mino)
 {
 	char	*grid;
 
 	grid = ft_strnew(21);
 	ft_bzero(grid, 21);
 	t_algo_recu(grid, 4);
-	
+
 	if ()
 
 }
