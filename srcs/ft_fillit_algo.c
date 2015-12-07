74681d70e0fefb3762e76178dea2ff5a8091b999
dolewski <dolewski@student.42.fr>
2015-12-07 12:33:05 +0100

    add comment

--- srcs/ft_fillit_algo.c
+++ srcs/ft_fillit_algo.c
@@ -11,16 +11,23 @@ static int		min_square(int t_minos_nb)
 	return (side_len);
 }
 
-static int		place_t_mino(char *grid, char **t_mino, int size, char mino_nb)
+static int		place_t_mino(char *grid, char *t_mino, int size, char mino_nb)
 {
-	grid = NULL;
-	t_mino = NULL;
-	size = 0;
-	mino_nb=0;
+	// pk mino_nb ??????? pas besoin on va pas parcourire la liste des mino on veux seulement celui en cour
+	// a la place de mino_nb -> l'emplacement atuelle de la grille pour eviter de la reparcourire depuis le debut
+	// verifie si il ya la place pour mettre le mino en cour
+	// a partir d'un point x de la grid
+	int		i;
+
+	i = emplacement_grid;
+	while (grid[i])
+	{
+
+	}
 	return (0);
 }
 
-char			*t_algo_r(char *grid, char **t_mino, int size)
+static char		*t_algo_r(char *grid, char **t_mino, int size)
 {
 	int		i;
 	int		j;
@@ -29,13 +36,20 @@ char			*t_algo_r(char *grid, char **t_mino, int size)
 	j = 0;
 	while (grid[i])
 	{
-		if (grid[i] == '#' /* il n'y a pas de # seulement des lettre  */ || /*fonction qui permet de placer les pieces return nope*/)
-			t_algo_r(&grid[i + 1], &t_mino[j], size);
-		if (/*fonction qui permet de placer les pieces return ok*/)
-			t_algo_r(&grid[i + 1], &t_mino[j + 1], size);
+		// On regarde si on est sur un mino ou si on a la place pour poser le mino en cour
+		if (grid[i] != '.' || place_t_mino(grid, t_mino[j], size, j) == 0)
+			t_algo_r(&grid[i + 1], t_mino, size);
+		// si il y'a la place on place le mino
+		else if (place_t_mino(grid, t_mino[j], size, j) == 1)
+			t_algo_r(&grid[i + 1], (t_mino + 1), size);
 	}
+	// si plus de place on augmente la grille
 	if (t_mino != NULL || grid[i] == '\0')
+	{
+		// void comment realouer de l'espace et recopier la grille dans cette espace
+		grid = ft_strnew(size + 1);
 		t_algo_r(&grid[0], &t_mino[j], size + 1);
+	}
 	return (NULL);
 }
 
